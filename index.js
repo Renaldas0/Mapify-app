e2a4dfa22767aabd4142cb76b08eb5f21a8cc966
Renaldas Bendikas <renaldasb1999@gmail.com>
2023-12-11 11:09:49 +0000

    add leaflet map api

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-'use strict';
+"use strict";
 
-// prettier-ignore
+
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
 const form = document.querySelector('.form');
@@ -10,3 +10,39 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function (position) {
+        const { latitude } = position.coords;
+        const { longitude } = position.coords;
+        console.log(`https://www.google.com/maps/@${latitude},${longitude},8.25z?entry=ttu`)
+
+        const coords = [latitude, longitude];
+
+        const map = L.map('map').setView(coords, 13);
+
+        L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+
+        map.on('click', (mapEvent) => {
+            const { lat, lng } = mapEvent.latlng;
+
+            L.marker([lat, lng])
+                .addTo(map)
+                .bindPopup(L.popup({
+                    maxWidth: 250,
+                    minWidth: 100,
+                    autoClose: false,
+                    closeOnClick: false,
+                    className: 'running-popup',
+
+                }))
+                .setPopupContent('Workout')
+                .openPopup();
+        });
+
+    }, function () {
+        alert('Could not get current position');
+    });
+};
